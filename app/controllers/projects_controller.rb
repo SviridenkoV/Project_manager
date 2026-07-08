2dd22c94cdcf8851f046ef0dec0d9b69b78b2331
Dragonpro761 <dragonpro762@yandex.ru>
2026-07-08 12:39:09 +0300

    Модели, полный CRUD для projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = current_user.projects
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Проект создан!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: "Проект обновлён!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Проект удалён!"
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,12 @@
+class Task < ApplicationRecord
+  belongs_to :project
+
+  enum status: {
+  to_do: 0,
+  in_progress: 1,
+  in_testing: 2,
+  rejected: 3,
+  done: 4
+}
+
+end
