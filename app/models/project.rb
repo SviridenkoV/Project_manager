58301359a9dd87ff79c35d94a60c8be631b11291
Dragonpro761 <dragonpro762@yandex.ru>
2026-07-19 22:44:53 +0300

    Added more validates to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, uniqueness: { scope: :user_id }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,10 @@ class Task < ApplicationRecord
   STATUSES = %w[to_do in_progress in_testing rejected done].freeze
   PRIORITIES = %w[critical high medium low].freeze
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 }, allow_blank: true
   validates :status, inclusion: { in: STATUSES }
   validates :priority, inclusion: { in: PRIORITIES }
+  validates :project, presence: true
+  validates :title, uniqueness: { scope: :project_id, message: "already exists in this project" }
 end
